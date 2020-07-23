585da24f133b0d119f9fc6449c6c5d00eecb972c
Hui Lin <huilinqc@gmail.com>
2020-07-23 01:39:55 -0400

    Updates

---
+++ precache-manifest.40baaee7eb7f35b913a6e57bbd67a442.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0722d94aa828252dbf72d65c30fe256",
+    "url": "/onlythefoxgodknows.github.io/puzzle/index.html"
+  },
+  {
+    "revision": "87e3d0e3188f9e164165",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/css/main.ad77144a.chunk.css"
+  },
+  {
+    "revision": "b601f290a0d94ef7d181",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/js/2.1d1b6572.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/js/2.1d1b6572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e3d0e3188f9e164165",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/js/main.b1dfd066.chunk.js"
+  },
+  {
+    "revision": "fbecbd3db9a17c9a20f9",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/js/runtime-main.df532c5d.js"
+  },
+  {
+    "revision": "362e2e5b2731d6c0fb99276e581eba4a",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/media/0.362e2e5b.jpg"
+  },
+  {
+    "revision": "b78a72052098b25ba8342f8a278c7ba3",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/media/1.b78a7205.jpg"
+  },
+  {
+    "revision": "ac0a00d706b7c376463f5d80b0a696c2",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/media/2.ac0a00d7.jpg"
+  },
+  {
+    "revision": "eb1695423648b64c24eb4c903b331630",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/media/3.eb169542.jpg"
+  },
+  {
+    "revision": "b982a43a7f05f44446ce582141119f74",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/media/5.b982a43a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/onlythefoxgodknows.github.io/puzzle/static/media/logo.5d5d9eef.svg"
+  }
+]);
